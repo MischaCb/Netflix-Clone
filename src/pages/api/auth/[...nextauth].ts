99cba467d3925e9506cb0377d831af73acf3c5ab
MischaCb <mihaela.corbu95@gmail.com>
2023-07-13 13:28:45 +0200

    test register and login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,44 +6,44 @@ import prismadb from "@/lib/prismadb";
 export default NextAuth({
   providers: [
     Credentials ({
-      id: "Credentials",
+      id: "credentials",
       name: "Credentials",
       credentials: {
         email: {
           label: "Email",
           type: "text",
         },  
-        passwword: {
+        password: {
           label: "Password",
           type: "password",
         }
       },
-     async authorize(credentials) {
-      if(!credentials?.email || !credentials?.passwword) {
-        throw new Error("Email and password required");
-      }
-
-      const user = await prismadb.user.findUnique({
-        where: {
-          email: credentials.email
+      async authorize(credentials) {
+        if(!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
         }
-      });
 
-      if(!user || !user.hashedPassword) {
-        throw new Error("Email does not exist");
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        });
+
+        if(!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+
+        const isCorrectPassword = await compare (
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if(!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
       }
-
-      const isCorrectPassword = await compare (
-        credentials.passwword,
-        user.hashedPassword
-      );
-
-      if(!isCorrectPassword) {
-        throw new Error("Incorrect password");
-      }
-
-      return user;
-     }
     })
   ],
   pages: {
